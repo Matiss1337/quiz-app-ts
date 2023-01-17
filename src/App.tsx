7e492823e1e3e7c568419a67c50c1e75ecd8b42e
Matiss <matissj1337@gmail.com>
2023-01-17 10:35:16 +0100

    added couple elements and api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
 import './App.css'
+import { QuestionCard } from './components/QuestionCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+const startTrivia = async () => {
+console.log("yo")
+}
 
-  return (
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+}
+
+const nextQuestion = () => {
+console.log("next question")
+}
+
+const imgLink: string = "https://images.unsplash.com/photo-1619897917857-f80c6adcf760?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+  
+return (
     <div className="App">
-
+<h1>REACT QUIZ</h1>
+<button className='start' onClick={startTrivia}>Start</button>
+<p className="score">Score:</p>
+<p className="loading">Loading Questions ...</p>
+<QuestionCard/>
+<button className="next" onClick={nextQuestion}>next question</button>
     </div>
   )
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const QuestionCard = () => {
+  return (
+    <div>QuestionCard</div>
+  )
+}
